93b0e4df1cf43509ba186a2f3809f67a12845df5
bilalyasar <bilalyasar45@gmail.com>
2015-05-08 18:38:50 +0300

    edited application

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 # file run.sh
 #!/bin/sh
-export CATALINA_OPTS="$CATALINA_OPTS -Dmember1=$member1 -Dmember2=$member2 -Dpublic_ip=$public_ip"
+export CATALINA_OPTS="$CATALINA_OPTS -Dmember1=$member1 -Dmember2=$member2 -Dpublic_ip=$public_ip -Dname=$name"
 catalina.sh run
